70d1b5dd0cf88557692bda2ba9754ebbe53d3173
Marx <marxborgesmachado@gmail.com>
2021-08-29 17:47:02 -0300

    Player move system

--- ProjectGameZombie/Assets/Scripts/Player/PlayerHandler.cs
+++ ProjectGameZombie/Assets/Scripts/Player/PlayerHandler.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerHandler : MonoBehaviour
 {
+	//Variable move player
+	[Header("Move player")]
     [SerializeField] private CharacterController characterController;
 	[SerializeField] private Transform cam;
 	private float turnSmoothTime = 0.15f;
 	float turnSmoothVelocity;
-	
 	public float speed = 0f;
 
+	//Variabel shooting player
+	[Header("Shooting player")]
+	[SerializeField] private Transform camTransform;
+	[SerializeField] private LayerMask layerMask;
+	[SerializeField] private CinemachineFreeLook cine;
 	void Update()
 	{
+		//Move player
 		float x = Input.GetAxis("Horizontal");
 		float z = Input.GetAxis("Vertical");
 
@@ -27,5 +35,14 @@ public class PlayerHandler : MonoBehaviour
 			Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 			characterController.Move(moveDir * speed * Time.deltaTime);
 		}
+
+		//Shooting player
+		RaycastHit hit;
+		if(Physics.Raycast(camTransform.position, camTransform.forward, out hit, 500f, layerMask))
+		{
+			if(Input.GetMouseButtonDown(0)) Destroy(hit.transform.gameObject);
+		}
+		if (Input.GetMouseButton(1)) cine.m_Lens.FieldOfView = 20;
+		else if(Input.GetMouseButtonUp(1)) cine.m_Lens.FieldOfView = 40;
 	}
 }
