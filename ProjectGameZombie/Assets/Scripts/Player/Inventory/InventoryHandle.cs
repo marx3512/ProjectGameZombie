db421adbfa8129c90e20a3f743757daf2e57c018
Marx <marxborgesmachado@gmail.com>
2021-09-18 10:08:14 -0300

    Inventory System

---
+++ ProjectGameZombie/Assets/Scripts/Player/Inventory/InventoryHandle.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Inventory.Code {
+    
+    public class InventoryHandle : MonoBehaviour
+    {
+        public GameObject inventoryObj;
+
+        private void Start()
+        {
+            inventoryObj.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                if (inventoryObj.activeSelf){
+                    ChangeBool(false);
+                    Cursor.lockState = CursorLockMode.Locked;
+                } 
+                else{
+                    Cursor.lockState = CursorLockMode.None;
+                    ChangeBool(true);
+                }
+            }
+        }
+
+        private void ChangeBool(bool cond){
+            inventoryObj.SetActive(cond);
+            Cursor.visible = cond;
+        }
+    }
+}
--- ProjectGameZombie/Assets/Scripts/Player/PlayerHandler.cs
+++ ProjectGameZombie/Assets/Scripts/Player/PlayerHandler.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Cinemachine;
 using Weapon;
+using Inventory.Code;
 
 namespace Player{
     public class PlayerHandler : MonoBehaviour
@@ -31,61 +32,68 @@ namespace Player{
 
 		//Munition
 		[HideInInspector] public int AmmoBag;
+
+        //Inventory
+        InventoryHandle inventoryHandle;
 		void Start()
         {
             health = playerStatus.Health;
+            inventoryHandle = GetComponent<InventoryHandle>();
         }
 
         void Update()
-        {
-            //Move player
-            #region Move player
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
+        {   
+            if(!inventoryHandle.inventoryObj.activeSelf){
+                //Move player
+                #region Move player
+                float x = Input.GetAxis("Horizontal");
+                float z = Input.GetAxis("Vertical");
 
-            Vector3 move = new Vector3(x, 0, z).normalized;
+                Vector3 move = new Vector3(x, 0, z).normalized;
 
-            if (move.magnitude >= 0.1f)
-            {
-                float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                anim.SetBool("MoveNotSight", true);
-                characterController.Move(moveDir * speed * Time.deltaTime);
-            }
-            else anim.SetBool("MoveNotSight", false);
-            #endregion
-            //Shooting player
-            #region Shot player
-            if (Input.GetMouseButtonDown(0))
-            {
-                RaycastHit hit;
-                if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, 50f, layerMask))
+                if (move.magnitude >= 0.1f)
                 {
-                    var target = hit.transform.gameObject.GetComponent<EnemyHandler>();
-                    if (target != null) target.health -= 1;
+                    float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                    Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                    anim.SetBool("MoveNotSight", true);
+                    characterController.Move(moveDir * speed * Time.deltaTime);
                 }
-                weaponsHandle.Shooting();
+                else anim.SetBool("MoveNotSight", false);
+                #endregion
+                //Shooting player
+                #region Shot player
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, 50f, layerMask))
+                    {
+                        var target = hit.transform.gameObject.GetComponent<EnemyHandler>();
+                        if (target != null) target.health -= 1;
+                    }
+                    weaponsHandle.Shooting();
+                }
+                if (Input.GetMouseButton(1))
+                {
+                    cine.m_Lens.FieldOfView = 20;
+                    cine.m_YAxis.m_MaxSpeed = 1f;
+                    cine.m_XAxis.m_MaxSpeed = 100.0f;
+                    //Animations
+                    anim.SetFloat("X", x);
+                    anim.SetFloat("Y", z);
+                }
+                else if (Input.GetMouseButtonUp(1))
+                {
+                    cine.m_Lens.FieldOfView = 40;
+                    cine.m_YAxis.m_MaxSpeed = 2.0f;
+                    cine.m_XAxis.m_MaxSpeed = 200.0f;
+                }
+                if (Input.GetKeyDown(KeyCode.R)) weaponsHandle.Reloading();
+                #endregion
             }
-            if (Input.GetMouseButton(1))
-            {
-                cine.m_Lens.FieldOfView = 20;
-                cine.m_YAxis.m_MaxSpeed = 1f;
-                cine.m_XAxis.m_MaxSpeed = 100.0f;
-                //Animations
-                anim.SetFloat("X", x);
-                anim.SetFloat("Y", z);
-            }
-            else if (Input.GetMouseButtonUp(1))
-            {
-                cine.m_Lens.FieldOfView = 40;
-                cine.m_YAxis.m_MaxSpeed = 2.0f;
-                cine.m_XAxis.m_MaxSpeed = 200.0f;
-            }
-            if(Input.GetKeyDown(KeyCode.R)) weaponsHandle.Reloading();
-            #endregion
+            
         }
     }
 }
